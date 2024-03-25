153b0997fef496b109b1bec462cb21854e18467b
Arbaz989 <157392475+Arbaz989@users.noreply.github.com>
2024-03-25 00:18:43 +0000

    how to save variables in the memory

--- 01-basics/test.js
+++ 01-basics/test.js
@@ -1,1 +1,17 @@
+const accountId = 2323
+let accountEmail = "aer@gmail.com"
+var accontPassword = "affff"
+let accountCity ;
+accountCash = 2323
+
+accountEmail = "a@gmail.com"
+accontPassword = "ar"
+accountCash = 111
+
+console.table([accountId,accountEmail,accontPassword,accountCash,accountCity])
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope 
+*/
+
 console.log("Arbaz")
