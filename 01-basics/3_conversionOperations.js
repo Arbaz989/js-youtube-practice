4bbf57342105ea336e703d1b3d6a116224b221fc
Arbaz989 <157392475+Arbaz989@users.noreply.github.com>
2024-03-25 01:07:36 +0000

    conversion of dataTypes

---
+++ 01-basics/3_conversionOperations.js
@@ -0,0 +1,28 @@
+let score = Boolean
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33avc" => NaN
+// true => 1 false => 0
+// null => 0
+
+let isUserLogged = "arbas"
+
+let booleanIsUserLogged = Boolean(isUserLogged)
+console.log(booleanIsUserLogged);
+
+// 1 => True 0 => False
+//"" => false
+// "arbas" => true
+
+let someNumber = 334
+
+let stringNumber =String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
