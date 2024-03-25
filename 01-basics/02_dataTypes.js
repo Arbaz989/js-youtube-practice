55e02686976de9d72c9913075c9eba53736342e7
Arbaz989 <157392475+Arbaz989@users.noreply.github.com>
2024-03-25 00:19:27 +0000

    learned basic dataTypes

---
+++ 01-basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treate all code as a newer version
+
+// alert(3+3) //we are using nodejs not browser it works on browser because it javascript engine is on browser
+
+console.log(3 +
+    3) // code  redeablity should be high
+    
+    let name = "Atbas"
+    let age = 21
+    let ciity;
+    let cashs = null
+
+                // PRIMITIVE DATATYPES //
+    // String => ""
+    // int => 2 to power 53
+    // bigint 
+    // boolean => True False
+    // nulll => stand alone 
+    // undefined
+    // symbol => unique
+
+    // object 
+
+    console.log(typeof age)
+    console.log(typeof null) //object
+    console.log(typeof undefined) //undefined
+
+
