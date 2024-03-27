27f5c2639890e06005dc39baf385290594d5c9f0
Arbaz989 <157392475+Arbaz989@users.noreply.github.com>
2024-03-27 06:36:35 +0000

    learned operations in js

--- 01-basics/3_conversionOperations.js
+++ 01-basics/3_conversionOperations.js
@@ -1,11 +1,11 @@
 let score = Boolean
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33avc" => NaN
@@ -15,7 +15,7 @@ console.log(valueInNumber);
 let isUserLogged = "arbas"
 
 let booleanIsUserLogged = Boolean(isUserLogged)
-console.log(booleanIsUserLogged);
+// console.log(booleanIsUserLogged);
 
 // 1 => True 0 => False
 //"" => false
@@ -24,5 +24,53 @@ console.log(booleanIsUserLogged);
 let someNumber = 334
 
 let stringNumber =String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+
+
+// **********************   OPERATIONS  *************************//
+let intValue = 3
+let negValue = -intValue
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2**5);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "hello"
+let str2 = "Arbaz"
+// let str3 = str1+str2
+// console.log(str3)
+// console.log("1"+2+3)
+// console.log(2+3+"3");
+// console.log((2+3)*3/4);
+// do not use this type of confusing code 
+
+console.log(+true)
+console.log(+"")
+let num1,num2,num3
+num1=num2=num3=2+2
+console.log(num1+num2+num3)
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+let x =3
+let y=x++;
+console.log(y);
+console.log(x);
+
+let xx = 3
+let yy = ++xx
+
+console.log(xx)
+console.log(yy)
+
+
+
+
