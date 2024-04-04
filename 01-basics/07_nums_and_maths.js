664175fceb137e0398cd7e0a12fe615ac254382f
Arbaz989 <157392475+Arbaz989@users.noreply.github.com>
2024-04-04 16:24:26 +0000

    num and math

--- 01-basics/07_nums_and_maths.js
+++ 01-basics/07_nums_and_maths.js
@@ -30,7 +30,11 @@ console.log(newGame.toLocaleString('en-IN'))
 
 console.log((Math.random()*10)+1)
 
+console.log(Math.floor(Math.random()*10)+1)
 
-
+const min = 10
+const max = 20
+ 
+console.log(Math.floor(Math.random() * (max-min+1)+min))
 
 
