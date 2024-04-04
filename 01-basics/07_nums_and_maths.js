9662605a12a778f4fde69d330f8712bd9a83a0a8
Arbaz989 <157392475+Arbaz989@users.noreply.github.com>
2024-04-04 16:23:52 +0000

    math and num

---
+++ 01-basics/07_nums_and_maths.js
@@ -0,0 +1,36 @@
+
+// *************NUMBERS**************************
+
+const score = 34
+console.log(score)
+const newScore = new Number(34)
+console.log(newScore)
+
+// 
+console.log(score.toString())
+console.log(newScore.toFixed(2))
+
+const game = 232.323232
+console.log(game.toPrecision(4))
+console.log(game.toExponential(3))
+
+const newGame = 100000
+console.log(newGame.toLocaleString('en-IN'))
+// ******************MATHS**************************
+
+//console.log(Math) //Math is an object 
+
+//console.log(Math.abs(-4))  // turn the value from  negativ to positive
+// console.log(Math.round(3.4))
+// console.log(Math.ceil(3.6))
+// console.log(Math.floor(3.4))
+// console.log(Math.min(3,4,4,4,4))
+// console.log(Math.max(4,4343,43434,34))
+
+
+console.log((Math.random()*10)+1)
+
+
+
+
+
